0e7c42d044abb63b0af8efcea081916b3e69776f
lobodner <lobodner@tsn.at>
2022-09-30 08:31:47 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My first GitHuboject");
+        System.out.println("My first GitHub Proooooooject");
     }
 }
